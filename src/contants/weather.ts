49e21822c30101cf9920151d194ce2d76412c565
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-01-12 16:56:21 +0900

    feat: Webフォント適用、アイコン情報と各種スタイルの定数作成(#3)

---
+++ src/contants/weather.ts
@@ -0,0 +1,14 @@
+const weatherIcon = {
+  sn: '/weather/Snow.png',
+  sl: '/weather/Sleet.png',
+  h: '/weather/Hail.png',
+  t: '/weather/Thunderstorm.png',
+  hr: '/weather/HeavyRain.png',
+  lr: '/weather/LightRain.png',
+  s: '/weather/Shower.png',
+  hc: '/weather/HeavyCloud.png',
+  lc: '/weather/LightCloud.png',
+  c: '/weather/Clear.png',
+};
+
+export { weatherIcon };
