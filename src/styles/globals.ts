b8091bea56409c82b91b1fc8009b9a73b813525e
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2025-02-24 14:28:32 +0900

    update: Prettierのアプデ 3.0.3 -> 3.5.2(#16)

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -7,8 +7,9 @@ export const globalStyle = css`
   body {
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    font-family:
+      -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+      Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     background-color: #f2f2f2;
   }
 
